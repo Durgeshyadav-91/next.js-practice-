b2f28b4e5e805ba6bfc2d4e133c3e3a09f02d71b
Durgesh Yadav <durgesh@vkss.tech>
2024-11-27 16:27:19 +0530

    changes

--- src/app/login/studentlogin/page.js
+++ src/app/login/studentlogin/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function() {
     return(
-        <div>
+        <div className="flex gap-4">
             <h1>Hello Students</h1>
             <Link href={"/login"}>Go to Login Page</Link><br/><br/>
             <Link href={"/"}>Go to Home Page</Link><br/><br/>
